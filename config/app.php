20ceefd0c12a030b39be11dbb080f54fb11465d9
Donie Prastyo <donie.viola@gmail.com>
2020-03-26 03:00:51 +0700

    Fixing bugs

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Package Service Providers...
          */
